805e9b10c3a3b2046e5330108194ab90d181ce8b
Dinesh Shrestha <dinesh.shrestha@mat-norge.no>
2024-11-13 21:40:39 +0100

    01basics react vite project

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+import Youtube from "./youtube";
+function App() {
+  return (
+    <>
+      <h2>Vite basic project</h2>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,5 @@
+function Youtube() {
+  return <h2>Youtube</h2>;
+}
+
+export default Youtube;
